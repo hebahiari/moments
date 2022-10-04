c2678bbe15b001c6a44b5a7ec66d9e737cf5dd5c
Heba Hiari <hebahiary+github@gmail.com>
2022-10-04 11:55:11 -0700

    right bar styling complete

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,7 +1,29 @@
 import "./rightbar.css"
+import {Celebration} from "@mui/icons-material"
 
 export default function Rightbar() {
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          < Celebration className="birthdayImg"/>
+          <span className="birthdayText">
+            <b> Heba A </b> and <b> 3 other friends </b> have a birthday today
+          </span>
+        </div>
+        <img src="assets/pictures/ad.jpg" className="rightbarAd" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/people/3.png" className="rightbarProfileImg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Fruitti H</span>
+          </li>
+        </ul>
+      </div>
+
+    </div>
   )
 }
