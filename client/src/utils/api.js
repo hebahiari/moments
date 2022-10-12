30fa9d6a6a69a9aaca49b2e6dc68cfb7ff49b67f
Heba Hiari <hebahiary+github@gmail.com>
2022-10-12 12:23:54 -0700

    added screenshots

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,70 +2,13 @@ import axios from "axios";
 //server's URL
 const API_BASE_URL = "http://54.193.51.33:8800/api";
 
-export async function getFollowingPosts(id) {
-  return await axios.get(`${API_BASE_URL}/posts/timeline/${id}`);
-}
-
-export async function getAllPosts() {
-  return await axios.get(`${API_BASE_URL}/posts/timeline/all`);
-}
-
+// to users
 export async function getUserById(id) {
   return await axios.get(`${API_BASE_URL}/users?userId=${id}`);
 }
 
 export async function getUserByUsername(username) {
   return await axios.get(`${API_BASE_URL}/users?username=${username}`);
-}
-
-export async function getUserPosts(username) {
-  return await axios.get(`${API_BASE_URL}/posts/profile/${username}`);
-}
-
-export async function loginCall(userCredentials, dispatch) {
-  dispatch({ type: "LOGIN_START" });
-  try {
-    const res = await axios.post(`${API_BASE_URL}/auth/login`, userCredentials);
-    localStorage.setItem("storedUser", JSON.stringify(res.data));
-    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
-  } catch (error) {
-    dispatch({ type: "LOGIN_FAILURE", payload: error });
-  }
-}
-
-export async function registerUser(user) {
-  return await axios.post("${API_BASE_URL}/auth/register", user);
-}
-
-export async function likeDislikePost(postId, userId) {
-  return await axios.put(`${API_BASE_URL}/posts/${postId}/like`, {
-    userId: userId,
-  });
-}
-
-export async function sendComment(comment) {
-  console.log({ comment });
-  return await axios.post(`${API_BASE_URL}/comments`, comment);
-}
-
-export async function getPostComments(postId) {
-  return await axios.get(`${API_BASE_URL}/comments/${postId}`);
-}
-
-export async function sharePost(post) {
-  return await axios.post(`${API_BASE_URL}/posts`, post);
-}
-
-export async function getPost(postId) {
-  return await axios.get(`${API_BASE_URL}/posts/${postId}`);
-}
-
-export async function uploadImage(data) {
-  return await axios.post(`${API_BASE_URL}/upload`, data);
-}
-
-export async function getFollowingUsers(userId) {
-  return await axios.get(`${API_BASE_URL}/users/following/${userId}`);
 }
 
 export async function getFollowersUsers(userId) {
@@ -84,14 +27,79 @@ export async function unfollowUser(userId, currentUserId) {
   });
 }
 
-export async function deletePost(postId, userId) {
-  return await axios.delete(`${API_BASE_URL}/posts/${postId}/${userId}`);
-}
-
 export async function findMatchingUsernames(username) {
   return await axios.get(`${API_BASE_URL}/users/search/${username}`);
 }
 
 export async function updateProfilePicture(img, userId) {
   return await axios.put(`${API_BASE_URL}/users/${userId}/img`, { img: img });
+}
+
+export async function getFollowingUsers(userId) {
+  return await axios.get(`${API_BASE_URL}/users/following/${userId}`);
+}
+
+// to posts
+export async function getFollowingPosts(id) {
+  return await axios.get(`${API_BASE_URL}/posts/timeline/${id}`);
+}
+
+export async function getAllPosts() {
+  return await axios.get(`${API_BASE_URL}/posts/timeline/all`);
+}
+
+export async function getUserPosts(username) {
+  return await axios.get(`${API_BASE_URL}/posts/profile/${username}`);
+}
+
+export async function likeDislikePost(postId, userId) {
+  return await axios.put(`${API_BASE_URL}/posts/${postId}/like`, {
+    userId: userId,
+  });
+}
+
+export async function sharePost(post) {
+  return await axios.post(`${API_BASE_URL}/posts`, post);
+}
+
+export async function getPost(postId) {
+  return await axios.get(`${API_BASE_URL}/posts/${postId}`);
+}
+
+export async function deletePost(postId, userId) {
+  return await axios.delete(`${API_BASE_URL}/posts/${postId}/${userId}`);
+}
+
+// to auth
+
+export async function loginCall(userCredentials, dispatch) {
+  dispatch({ type: "LOGIN_START" });
+  try {
+    const res = await axios.post(`${API_BASE_URL}/auth/login`, userCredentials);
+    localStorage.setItem("storedUser", JSON.stringify(res.data));
+    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+  } catch (error) {
+    dispatch({ type: "LOGIN_FAILURE", payload: error });
+  }
+}
+
+export async function registerUser(user) {
+  return await axios.post("${API_BASE_URL}/auth/register", user);
+}
+
+// to comments
+
+export async function sendComment(comment) {
+  console.log({ comment });
+  return await axios.post(`${API_BASE_URL}/comments`, comment);
+}
+
+export async function getPostComments(postId) {
+  return await axios.get(`${API_BASE_URL}/comments/${postId}`);
+}
+
+// to upload
+
+export async function uploadImage(data) {
+  return await axios.post(`${API_BASE_URL}/upload`, data);
 }
