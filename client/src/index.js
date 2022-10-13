33d8067d7589953105be8b534f03021d254e8284
Heba Hiari <hebahiary+github@gmail.com>
2022-10-13 10:43:43 -0700

    heroku client

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,11 @@ import { AuthContextProvider } from "./context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthContextProvider>
+  <AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthContextProvider>
   // </React.StrictMode>
+  //
 );
