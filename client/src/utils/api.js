7a5d818e0d626a38440c851d689edfc95f0c6362
Heba Hiari <hebahiary+github@gmail.com>
2022-10-14 09:55:49 -0700

    updated api url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 //server's URL
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-// "https://petsgram-website-backend.herokuapp.com";
+const API_BASE_URL =
+  // process.env.REACT_APP_API_BASE_URL;
+  "https://petsgram-website-backend.herokuapp.com";
 
 // to users
 export async function getUserById(userId) {
