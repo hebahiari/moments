81a3a1a06ac6c9a9b841df3a4908b64af98e65be
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-04-26 20:08:18 -0700

    updated api url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 //server's URL
-const API_BASE_URL = "http://localhost:8800/api";
-// process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// "http://localhost:8800/api";
 
 // to users
 export async function getUserById(userId) {
