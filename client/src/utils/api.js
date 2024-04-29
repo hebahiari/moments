b3aa8bc29c43ba75b076a8da959bb365ed9b1154
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-04-29 14:48:02 -0700

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //server's URL
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://petsgram.hebahiari.com/api';
 // "http://localhost:8800/api";
 
 // to users
